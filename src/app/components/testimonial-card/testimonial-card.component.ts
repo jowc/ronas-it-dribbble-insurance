e7bf68f7fe37af7bd77df55dae81243d1626cba8
Joseph Chikeme <jchikeme@gmail.com>
2023-06-04 17:46:35 +0100

    Added Swiper Js

--- src/app/components/testimonial-card/testimonial-card.component.ts
+++ src/app/components/testimonial-card/testimonial-card.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OnInit, Component, signal } from '@angular/core';
 import { MpButtonDirective } from 'src/app/shared/directive/button-full.directive';
+
+import Swiper, { Pagination, Autoplay } from 'swiper';
+
+interface cardInterface {
+  img_url: string;
+  person_name: string;
+  job_title: string;
+  comment: string;
+}
 
 @Component({
   selector: 'app-testimonial-card',
   standalone: true,
-  imports: [MpButtonDirective],
+  imports: [CommonModule, MpButtonDirective],
   templateUrl: './testimonial-card.component.html',
   styleUrls: ['./testimonial-card.component.scss'],
 })
-export class TestimonialCardComponent {}
+export class TestimonialCardComponent implements OnInit {
+  testimonials = signal<cardInterface[]>([
+    {
+      img_url: 'assets/guy 1.jpg',
+      person_name: 'Emito Usman',
+      job_title: 'The agency founder',
+      comment:
+        "Not all insurance are the same. That's why agency know that your insurance should be customised to suit your situation.",
+    },
+    {
+      img_url: 'assets/smiley girl.jpg',
+      person_name: 'Sally Uche',
+      job_title: 'Techprenuer',
+      comment:
+        "With over twenty years of industry experience. 'Darcy Insurance Product' has learnt a lot. The one thing we know for sure is that we are all about you. If you find searching for insurance frustrating we are here to help you",
+    },
+    {
+      img_url: 'assets/happy couple.jpg',
+      person_name: 'Oluwatobi Chiobi',
+      job_title: 'Techprenuer',
+      comment:
+        'The one thing we know for sure is that we are all about you. If you find searching for insurance frustrating we are here to help you. With over twenty years of industry experience.',
+    },
+  ]);
+
+  ngOnInit(): void {
+    new Swiper('.swiper', {
+      modules: [Pagination, Autoplay],
+      loop: true,
+      grabCursor: true,
+      autoplay: {
+        delay: 8500,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  }
+}
