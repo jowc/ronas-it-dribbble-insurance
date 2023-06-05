27f49019b17c42b41f07a452b7ae03cae1d8880d
Joseph Chikeme <jchikeme@gmail.com>
2023-06-05 02:06:44 +0100

    Added animation to image component

--- src/app/components/image-section/image-section.component.ts
+++ src/app/components/image-section/image-section.component.ts
@@ -1,6 +1,12 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MpButtonDirective } from 'src/app/shared/directive/button-full.directive';
 
+import { gsap } from 'gsap';
+
+const tl = gsap.timeline({
+  defaults: { duration: 1, ease: 'power1.inOut', stagger: 0.4 },
+});
+
 @Component({
   selector: 'app-image-section',
   standalone: true,
@@ -14,16 +20,38 @@ import { MpButtonDirective } from 'src/app/shared/directive/button-full.directiv
 export class ImageSectionComponent implements AfterViewInit {
   @ViewChild('cardCircle', { static: true })
   circleImage!: ElementRef<HTMLDivElement>;
+  @ViewChild('imageSection', { static: true })
+  imageSection!: ElementRef<HTMLDivElement>;
 
   observer = new IntersectionObserver(
     (entries) => {
-      if (entries[0].isIntersecting)
-        entries[0].target.classList.add('mp-height');
+      if (entries[0].isIntersecting) {
+        this.startAnimation();
+        this.observer.unobserve(this.imageSection.nativeElement);
+      }
     },
-    { threshold: 1, rootMargin: '' }
+    { threshold: 0, rootMargin: '-45px' }
   );
 
   ngAfterViewInit(): void {
-    this.observer.observe(this.circleImage.nativeElement);
+    this.observer.observe(this.imageSection.nativeElement);
   }
+
+  startAnimation = () => {
+    tl.from(this.imageSection.nativeElement, { opacity: 0 })
+      .from('.mp-image-1', { scale: 0, opacity: 0 })
+      .fromTo(
+        this.circleImage.nativeElement,
+        { height: '0%', opacity: 0 },
+        {
+          height: '100%',
+          opacity: 1,
+          position: 'absolute',
+          bottom: 0,
+          duration: 2,
+        },
+        '-=1'
+      )
+      .from('.mp-image-3', { scale: 0, opacity: 0 });
+  };
 }
